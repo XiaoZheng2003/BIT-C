ab73411e2e24885d565f28746551eec4d098b4cd
XiaoZheng2003 <1345486584@qq.com>
2022-05-18 22:14:58 +0800

    Update codes.

--- Week 13/编码（选作）.c
+++ Week 13/编码（选作）.c
@@ -1,23 +1,34 @@
 #include<stdio.h>
 #include<string.h>
+int c[30][30];
 int C(int n,int m){
-	int ans=1;
-	for(int i=0;i<m;i++){
-		ans*=26-i;
-		ans/=i+1;
-	}
-	return ans;
-}
+	if(!m||n==m)	return 1;
+	return c[n][m]?c[n][m]:(c[n][m]=C(n-1,m)+C(n-1,m-1));
+} 
 int main()
 {
-	int n;
-	scanf("%d",&n);
-	while(n--){
+	int t;
+	scanf("%d",&t);
+	while(t--){
 		char str[15];
 		scanf("%s",str);
-		int n=strlen(str),ans=0,cnt=0;
-		for(int i=n-1;i>=0;i--)
-			ans+=(str[i]-'a'+2+i-n)*(cnt+=C(26,i));
+		int n=strlen(str),ans=1,flag=0;
+		for(int i=1;i<n;i++)
+			if(str[i-1]>=str[i]){
+				puts("0");
+				flag=1;
+				break;
+			}
+		if(flag)	continue;
+		for(int i=1;i<n;i++)
+			ans+=C(26,i);
+		for(int i=0;i<n;i++){
+			char ch=(!i)?'a':str[i-1]+1;
+			while(ch<=str[i]-1){
+				ans+=C('z'-ch,n-1-i);
+				ch++;
+			}
+		}
 		printf("%d\n",ans);
 	}
 }
