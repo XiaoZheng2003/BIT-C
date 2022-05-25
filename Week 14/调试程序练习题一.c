2e5426f03ecc60051eea7663a6617cf26a8ecd45
XiaoZheng2003 <1345486584@qq.com>
2022-05-25 18:43:19 +0800

    Update codes.

--- Week 14/调试程序练习题一.c
+++ Week 14/调试程序练习题一.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//*---*:为修改原因 
 void reverse(char *str,int i,int j){   
     char temp;
     if(i<j){
@@ -17,7 +18,7 @@ int main()
     while(s[n]!='\0')
         n++;
     //reverse(s,0,n);
-    reverse(s,0,n-1);	//传入的字符串应不含'\0' 
+    reverse(s,0,n-1);	//*传入的字符串应不含'\0'*
     puts(s);
     return 0;
 }
